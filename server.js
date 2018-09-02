c68c3e15511e95358014a200d8070f2aef28d5fd
Oluseyi Anani <anasey2018@gmail.com>
2018-09-02 14:51:03 +0100

    server setup files

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 import express from 'express';
+import path from 'path';
+import route from './controllers/routes';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', route.homePage);
+app.get('/syncvideo', route.sync);
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log('Now listening on port ', port);
+});
